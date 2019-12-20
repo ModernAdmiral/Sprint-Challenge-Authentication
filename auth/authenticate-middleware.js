68a6cfd496390c9a10dff91dce1a702fc50d6c6d
Jackson McComas <jackson.mccomas@gmail.com>
2019-12-20 12:44:39 -0600

    created authentication

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,23 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+const secret = require("../config/secret");
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "forbidden!" });
+      } else {
+        req.decodedJwt = decodedToken;
+        console.log("decoded token", req.decodedJwt);
+
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "you shall not pass!" });
+  }
 };
